3fcde9f99b93ed4bd3a24f8bd8844eb5e101fd6c
gopal221296 <gopalakrishnan.v@unacademy.com>
2019-11-24 20:19:49 +0530

    first commit

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next) {
+  const { originalname, mimetype, size } = req.file;
+  return res.send({
+    name: originalname,
+    type: mimetype,
+    size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
